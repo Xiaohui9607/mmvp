387df06f96e11244f943b64280988ea268587692
golfgang <joy.cxh0726@gmail.com>
2020-03-07 10:33:13 -0500

    run.sh

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ for BEHAVIOR in ${BEHAVIOR_ARRAY[@]};do
   python ./data/make_data.py --behavior $BEHAVIOR
   for VARIABLE in 1
   do
-    python ./train.py --output_dir weight_use_haptic_$VARIABLE --use_haptic
-    python ./train.py --output_dir weight_baseline_$VARIABLE
+    python ./train.py --output_dir weight_use_haptic_$VARIABLE_$BEHAVIOR --use_haptic --use_behavior --use_audio
+    python ./train.py --output_dir weight_baseline_$VARIABLE_$BEHAVIOR
     rm -r ../data/CY101NPY
   done
 done
