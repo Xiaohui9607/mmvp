894771289eceb370f5bdaa7a173cb043ce3d179f
Ramtin Hosseini <ramtin@Ramtins-MacBook-Pro.local>
2020-03-07 11:08:08 -0500

    fixed variables

--- run.sh
+++ run.sh
@@ -11,10 +11,11 @@
 declare -a BEHAVIOR_ARRAY=('crush' 'poke' 'push')
 for BEHAVIOR in ${BEHAVIOR_ARRAY[@]};do
   python ./data/make_data.py --behavior $BEHAVIOR
-  for VARIABLE in 1
+  for VARIABLE in 1 2 3 4 5
   do
-    python ./train.py --output_dir weight_use_haptic_$VARIABLE_$BEHAVIOR --use_haptic --use_behavior --use_audio
-    python ./train.py --output_dir weight_baseline_$VARIABLE_$BEHAVIOR
+    python ./train.py --output_dir weight_use_haptic_audio_$(VARIABLE)_$(BEHAVIOR) --use_haptic --use_behavior --use_audio
+    python ./train.py --output_dir weight_use_haptic_$(VARIABLE)_$(BEHAVIOR) --use_haptic --use_behavior
+    python ./train.py --output_dir weight_baseline_$(VARIABLE)_$(BEHAVIOR)
     rm -r ../data/CY101NPY
   done
 done
