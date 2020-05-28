b730e1215017495d0f3571d7d38034326d8fa1db
Ramtin Hosseini <ramtin.hosseini@tufts.edu>
2020-05-28 06:43:35 -0400

    ensemble added (plot updated, script modified)

--- results/plot_group_bar_plot.py
+++ results/plot_group_bar_plot.py
@@ -17,6 +17,14 @@ behaviors = ['crush', 'lift', 'grasp', 'shake', 'push', 'tap', 'hold', 'drop', '
 settings = ['baseline', 'haptic', 'haptic_audio', 'haptic_audio_vibro']
 #xlabels = ['Finn et al.', 'vision+haptic', 'vision+haptic\n+audio', 'vision+haptic\n+audio+vibro']
 plist = []
+
+# add ensemble
+dfs_baseline = []
+dfs_haptic= []
+dfs_haptic_audio = []
+dfs_haptic_audio_vibro = []
+# add ensemble
+
 for i, behavior in enumerate(behaviors):
     xs = []
     ys = []
@@ -24,6 +32,17 @@ for i, behavior in enumerate(behaviors):
     for j, a_setting in enumerate(settings):
         df = pd.read_csv(os.path.join(path, a_setting, '{}.csv'.format(behavior)))
         dfs.append(df.iloc[:, 2])
+        
+        # add ensemble
+        if a_setting =='baseline':
+            dfs_baseline.extend(df.iloc[:,2])
+        elif a_setting == 'haptic':
+            dfs_haptic.extend(df.iloc[:, 2])
+        elif a_setting == 'haptic_audio':
+            dfs_haptic_audio.extend(df.iloc[:, 2])
+        else:
+            dfs_haptic_audio_vibro.extend(df.iloc[:, 2])
+        # add ensemble
 
     p = pd.DataFrame(dfs).T
     p.columns=settings
@@ -33,8 +52,33 @@ for i, behavior in enumerate(behaviors):
     plist.append(p1)
 
 p = pd.concat(plist, axis=0)
+
+# add ensemble
+p_baseline = pd.DataFrame(dfs_baseline)
+p_baseline.columns=['baseline']
+p_baseline = p_baseline.melt(var_name='setting', value_name='SSIM')
+p_baseline['behavior']= 'ensemble'
+
+p_haptic = pd.DataFrame(dfs_haptic)
+p_haptic.columns=['haptic']
+p_haptic = p_haptic.melt(var_name='setting', value_name='SSIM')
+p_haptic['behavior']= 'ensemble'
+
+p_haptic_audio = pd.DataFrame(dfs_haptic_audio)
+p_haptic_audio.columns=['haptic_audio']
+p_haptic_audio = p_haptic_audio.melt(var_name='setting', value_name='SSIM')
+p_haptic_audio['behavior']= 'ensemble'
+
+p_haptic_audio_vibro = pd.DataFrame(dfs_haptic_audio_vibro)
+p_haptic_audio_vibro.columns=['haptic_audio_vibro']
+p_haptic_audio_vibro = p_haptic_audio_vibro.melt(var_name='setting', value_name='SSIM')
+p_haptic_audio_vibro['behavior']= 'ensemble'
+# add ensemble
+
+p = pd.concat([p, p_baseline, p_haptic, p_haptic_audio, p_haptic_audio_vibro])
+
 g = sns.catplot(x="behavior", y="SSIM", hue="setting", data=p, kind="bar",
-                col_order=['blue', 'orange', 'green', 'red'], aspect=2, legend=False, ci='sd')
+                col_order=['blue', 'orange', 'green', 'red'], aspect=3, legend=False, ci='sd')
 g.despine(left=True)
 
 plt.ylim([0.7, 1.0])
@@ -45,7 +89,7 @@ plt.ylabel('SSIM', fontsize=16)
 handles, _ = plt.gca().get_legend_handles_labels()
 labels = ['Finn et al. (vision)', 'vision+haptic', 'vision+haptic+audio', 'vision+haptic+audio+vibro']
 order = [3,2,1,0]
-plt.legend([handles[idx] for idx in order],[labels[idx] for idx in order])
+plt.legend([handles[idx] for idx in order],[labels[idx] for idx in order], loc='upper left')
 
 plt.savefig('sep_behave_group_bar.png', dpi=300)
 plt.show()
