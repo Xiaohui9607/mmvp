806f56cf31d055b83d1155c62e144b68c24cd219
Ramtin Hosseini <ramtin.hosseini@tufts.edu>
2020-05-30 08:48:24 -0400

    minor changes in plots

--- results/plot_all_with_std.py
+++ results/plot_all_with_std.py
@@ -7,13 +7,12 @@ paths = {
     'PSNR':'Experiement_object_based_all_behaviors/psnr_all_with_std.csv',
     'SSIM': 'Experiement_object_based_all_behaviors/ssim_all_with_std.csv'
 }
+sns.set(style="whitegrid")
 
-sns.set(style="darkgrid")
-
-plt.rcParams.update({'font.size': 20})
+plt.rcParams.update({'font.size': 16})
 plt.rc('ytick', labelsize=14)
 plt.rc('xtick', labelsize=14)
-f, axes = plt.subplots(1, 2, figsize=(13.5,6)) #figsize=(13.5,4.5)
+f, axes = plt.subplots(1, 2, figsize=(13.5,4.5)) #figsize=(13.5,4.5)
 for i, metric in enumerate(paths):
     df = pd.read_csv(paths[metric])
     if i == len(paths)-1:
@@ -26,9 +25,9 @@ for i, metric in enumerate(paths):
         axes[0].errorbar(x=range(4, 20), y=df.use_haptic_audio_mean, yerr=df.use_haptic_audio_std, marker="o", color='green')
         axes[0].errorbar(x=range(4, 20), y=df.use_haptic_mean, yerr=df.use_haptic_std, marker="o", color='orange')
         axes[0].errorbar(x=range(4, 20), y=df.baseline_mean, yerr=df.baseline_std, marker=8, color='blue')
-    axes[i].set_ylabel(metric, fontsize = 20)
-    axes[i].set_xlabel("Time step", fontsize = 20)
-    axes[i].set_title("Heldout set reconstruction evaluation", fontsize = 20)
+    axes[i].set_ylabel(metric, fontsize = 16)
+    axes[i].set_xlabel("Time step", fontsize = 16)
+    axes[i].set_title("Heldout set reconstruction evaluation", fontsize = 18)
     # plt.xlabel("# frames")
     axes[i].xaxis.set_major_locator(ticker.MultipleLocator(2))
     if metric == 'SSIM':
